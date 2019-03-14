e16adf1098ebebac6121993e87cfb7124b28020c
Iris <royalvald@sina.com>
2019-03-14 10:47:34 +0800

    new changes

---
+++ FirstDemo/test.vue
@@ -0,0 +1,17 @@
+Vue.component('Home', {template:'<div>Hello world</div>'})
+Vue.component("Left", {template:'<div>welcome</div>'})
+Vue.component("Right", {template:'<div>Dear</div>'})
+
+new Vue({
+    el:'demo1',
+    data:{
+        currentTab:'Home',
+        tabArrays:['Home','Left','Right']
+    },
+    computed: {
+        currentSelection:function()
+        {
+            return currentTab;
+        }
+    },
+})
