4ab263e68ada7cb42d74211ba42bca76dff63520
Iris <royalvald@sina.com>
2019-03-14 19:14:31 +0800

    new changes

---
+++ FistItesm/input-number.js
@@ -0,0 +1,22 @@
+Vue.component('input-number',
+{
+    template:'\
+    <div class="input-number">\
+    \
+    </div>',
+    props:{
+        max:{
+            type:Number,
+            default:Infinity
+        },
+        min:{
+            type:Number,
+            default:-Infinity
+        },
+        value:{
+            type:Number,
+            default:0
+        }
+    }
+}
+);
